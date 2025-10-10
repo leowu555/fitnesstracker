f951f6b87cacb121fbfcb3d88d94443aa9320e0f
Leo Wu <ywu156@student.ubc.ca>
2025-10-10 02:06:25 -0700

    Implemented the WorkoutList class.

--- src/main/model/WorkoutList.java
+++ src/main/model/WorkoutList.java
@@ -2,38 +2,55 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class WorkoutList {
     private List<String> workoutList;
 
-/*
- * EFFECTS: Construct an empty workout list that the user will complete.
- */
-public WorkoutList() {
-    this.workoutList = new ArrayList<>();
+    /*
+     * EFFECTS: Construct an empty workout list that the user will complete.
+     */
+    public WorkoutList() {
+        this.workoutList = new ArrayList<>();
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: Add the workout name to the workout list.
+     */
+    public void addWorkout(String workoutName) {
+        workoutList.add(workoutName);
+    }
+
+    /*
+     * EFFECTS: Check if the workout name is already in the list,
+     * if not in the list, return false, if in the list, return true.
+     */
+    public boolean isWorkoutInList(String workoutName) {
+        for(int i = 0; i<workoutList.size();) {
+            if(workoutList.contains(workoutName)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: Remove the workoutname of the workout that is completed from the
+     * workoutList.
+     */
+    public void removeWorkout(String workoutName) {
+        workoutList.remove(workoutName);
+    }
+
+    /*
+     * REQUIRES: the list size is non-zero.
+     * EFFECTS: Returns the size of the list.
+     */
+    public int size() {
+        return workoutList.size();
+    }
+
 }
-
-/*
- * MODIFIES: this
- * EFFECTS: Add the workout name to the workout list
- */
-public void addWorkout(String workoutName) {
-}
-
-/*
- * EFFECTS: Check if the workout name is already in the list, 
- * if not in the list, return false, if in the list, return true.
- */
-public boolean isWorkoutInList(String workoutName) {
-    return false;
-}
-
-/*
- * MODIFIES: this
- * EFFECTS: Remove the unwanted workout from the list.
- */
-public void removeWorkout(String workoutName) {
-}
-
-}
-
-
