d2c012b16340c2a3971a822c8d8eb99242d32bdd
Leo Wu <ywu156@student.ubc.ca>
2025-10-10 02:07:47 -0700

    Implemented the Goals class.

--- src/main/model/Goals.java
+++ src/main/model/Goals.java
@@ -2,49 +2,52 @@ package model;
 
 public class Goals {
     private String goals;
-    private int weeklyCalorieLimit;
     private int proteinGoals;
-    private int currentCalories;
     private int currentProtein;
 
-/*
- * REQUIRES: Goals need to have non-zero string length.
- * EFFECTS: Constructs the user goals for exercise and nutrition.
- */
+    /*
+     * REQUIRES: Goals need to have non-zero string length.
+     * EFFECTS: Constructs the user goals for exercise and nutrition.
+     */
     public Goals(String goals) {
         this.goals = goals;
     }
 
     public void setGoals(String newGoals) {
-
+        this.goals = newGoals;
     }
 
     public String getGoals() {
         return goals;
     }
 
-    public void setWeeklyCalorieLimit(int limit) {
+    public int getCurrentProtein() {
+        return currentProtein;
     }
 
     public void setProteinGoals(int grams) {
+        this.proteinGoals = grams;
     }
 
-/*
- * MODIFIES: this
- * EFFECTS: adds the protein amount to the current protein level, 
- * updating the current protein intake amount.
- */
-    public void addProtein (int amount) {
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds the protein amount to the current protein level,
+     * updating the current protein intake amount.
+     */
+    public void addProtein(int amount) {
+        this.currentProtein += amount;
     }
 
-/*
- * REQUIRES: currentCalories > 0, weeklyCalorieLimit > 0, currentProtein > 0, 
- * protein Goals > 0.
- * EFFECTS: Checks if the goals are met, produce true of a goal is met, if a goal is not met, produce false.
- */
+    /*
+     * REQUIRES: currentProtein > 0, proteinGoals > 0.
+     * EFFECTS: Checks if the goals are met, produce true of a goal is met, if a
+     * goal is not met, produce false.
+     */
     public boolean isGoalmet() {
+        if (currentProtein >= proteinGoals) {
+            return true;
+        }
         return false;
     }
 
 }
-
